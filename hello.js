b2ef0da039752feaa6b7669ff2b28f442a681431
Praveen <kodadala.praveen@gmail.com>
2020-02-13 16:21:40 +0530

    connected to mongodb

--- hello.js
+++ hello.js
@@ -1,9 +1,38 @@
 
+let wiki = require ('./wiki.js');
 let express = require('express');
 let app = express();
-const hostname = 'localhost';
-const port = 3000;
-let wiki = require ('./wiki.js');
+let  port = 3000;
+let logger = require('morgan');
+
+app.use(logger('dev'));
+
+let MongoClient = require('mongodb').MongoClient;
+
+
+//connection to mongo
+
+//https://expressjs.com/en/guide/database-integration.html
+//https://docs.mongodb.com/manual/mongo/
+
+
+
+const url = "mongodb://localhost:27017/animals"
+
+MongoClient.connect(url, (err, client)=>{
+if(err) throw err;
+console.log("Database created!");
+
+let  db = client.db('onedb');
+  //  db.collection('soal').find().toArray(function(err, result){
+  //    if(err) throw err;
+  //    console.log(result);
+  //    client.close();
+  //  });
+
+
+})
+
 
 
 
@@ -21,6 +50,14 @@ app.get('/', (req, res)=>{
   }, 3000);
   console.log('2nd');
 }),
+
+
+
+// app.use( function(err, req, res, next) {
+//   console.error(err.stack);
+//   res.status(500).send('Something broke!');
+// });
+
 
 
 
---
+++ readme.txt
@@ -0,0 +1,4 @@
+//connection to mongo
+
+//https://expressjs.com/en/guide/database-integration.html
+//https://docs.mongodb.com/manual/mongo/
